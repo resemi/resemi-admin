6ffbeacf6d2cf7acf2356ef43ea324bcace8c206
Anguer <191030830@qq.com>
2022-05-17 17:28:48 +0800

    fix: config windi colors by semi tokens

--- src/pages/admin/settings.tsx
+++ src/pages/admin/settings.tsx
@@ -3,12 +3,8 @@ import React from 'react';
 export default function Page() {
   return (
     <>
-      <div className="p-25px text-red-400 dark:text-green-400">Settings</div>
-      <div className="hover:(bg-gray-400 font-medium) bg-white font-light">asdasdsadsadsa</div>
-
-      <button type="button" className="btn btn-green">
-        Button
-      </button>
+      <div className="p-md text-red-4 dark:text-green-4">Settings</div>
+      <div className="hover:(bg-grey-4 font-medium) bg-green-4 font-light">asdasdsadsadsa</div>
     </>
   );
 }
