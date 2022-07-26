06565a1b92476fcec1e45f75e003cfec7e843fb2
anguer <191030830@qq.com>
2022-07-26 14:09:49 +0800

    fix(auth): small fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { withAuth } from 'next-auth/middleware';
 
 export default withAuth(
-  function middleware(req) {
-    console.log('#withAuth', req.nextauth.token);
-  },
+  // function middleware(req) {
+  //   console.log('#withAuth', req.nextauth.token);
+  // },
   {
     callbacks: {
       // authorized: ({ token }) => token?.role === 'admin',
@@ -13,5 +13,5 @@ export default withAuth(
 
 // Supports both a single string value or an array of matchers
 export const config = {
-  matcher: ['/admin'],
+  matcher: ['/admin/:path*'],
 };
--- src/pages/www/register.tsx
+++ src/pages/www/register.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Register() {
   return (
     <div>
-      Register<Link href="/">To Dashboard</Link>
+      Register<Link href="/admin/dashboard">To Dashboard</Link>
       Register<Link href="/admin/settings">To Settings</Link>
     </div>
   );
