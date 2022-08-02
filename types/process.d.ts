05c75bb85f76a86df2958a7d980159120b9678b4
anguer <191030830@qq.com>
2022-08-02 14:01:50 +0800

    chore: clean env

--- types/process.d.ts
+++ types/process.d.ts
@@ -2,7 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     APP_ENV: 'development' | 'production' | 'staging';
     NEXT_PUBLIC_BASE_API: string;
-    SITE_URL: string;
 
     NEXTAUTH_URL: string;
     NEXTAUTH_SECRET: string;
